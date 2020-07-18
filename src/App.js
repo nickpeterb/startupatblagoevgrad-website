a8d1e4a5873eb6ab7d47f294f3d30c29becc94fa
Nick Bruhnev <nicholaspeterb@gmail.com>
2020-07-18 01:49:59 -0500

    added nav, masthead, and about

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
 
+import StartUPNav from './components/StartUPNav.js';
+import Masthead from './components/Masthead.js';
+import About from './components/About.js';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      
+    <StartUPNav />
+
+    <Masthead />
+
+    <About />
+
+    <div className="testimonials" style={{height:"60vh"}}></div>
+
     </div>
   );
 }

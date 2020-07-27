f81cd4a1b04bbd7c3fc89f645044004240f2c92d
Nick Bruhnev <nicholaspeterb@gmail.com>
2020-07-27 14:33:43 -0500

    dynamic css

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -12,17 +12,17 @@ import societeGenerale from '../images/sponsors/societe-generale.png';
 import superhosting from '../images/sponsors/superhosting.png';
 import sutherland from '../images/sponsors/sutherland.png';
 
-const sponsorImgs = [
-    cocaCola,
-    redBull,
-    superhosting,
-    societeGenerale,
-    musalaSoft,
-    sutherland,
-    ahmadTea,
-	nordics,
-	pirinGolf
-];
+const sponsors = [
+    { className: "coca-cola", img: cocaCola },
+    { className: "red-bull", img: redBull },
+    { className: "superhosting", img: superhosting },
+    { className: "societe-generale", img: societeGenerale },
+    { className: "musala-soft", img: musalaSoft },
+    { className: "sutherland", img: sutherland },
+    { className: "nordics", img: nordics },
+    { className: "ahmad-tea", img: ahmadTea },
+    { className: "pirin-golf", img: pirinGolf }
+]
 
 export default function Sponsors() {
 	return (
@@ -33,9 +33,9 @@ export default function Sponsors() {
 				</h2>
 
 				<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4">
-					{sponsorImgs.map((sponsor, index) => (
+					{sponsors.map((sponsor, index) => (
 						<div class="col m-4">
-							<img src={sponsor} class="sponsor-img" alt=""></img>
+							<img src={sponsor.img} class={`sponsor-img ${sponsor.className}`} alt=""></img>
 						</div>
 					))}
 				</div>

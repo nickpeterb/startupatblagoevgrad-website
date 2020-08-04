c25c555d9e5ba3626d241a1ad9c0ccb536dd3f88
Nick Bruhnev <nickbruhnev@macbook-air.lan>
2020-08-04 17:03:37 -0500

    added each dept with team members in dynamic carousels

--- src/components/Team.js
+++ src/components/Team.js
@@ -4,34 +4,101 @@ import MediaQuery from 'react-responsive';
 
 import './styles/Team.css';
 
+//The Board
 import velko from '../images/team/velko.png';
-import nick from '../images/team/nick.png';
-import fatma from '../images/team/fatma.png';
-import boyan from '../images/team/boyan.png';
-import emma from '../images/team/emma.png';
-import deni from '../images/team/deni.png';
 import elito from '../images/team/elito.png';
-import flavia from '../images/team/flavia.png';
+import armela from '../images/team/armela-temp.png'; //need actual picture
+import stan from '../images/team/stan.png';
+
+//Speakers
+import aneta from '../images/team/aneta.png';
+import dari from '../images/team/dari.png';
+import fatma from '../images/team/fatma.png';
+import karolina from '../images/team/karolina.png';
+import key from '../images/team/key.png';
+import nadiaT from '../images/team/nadia-t.png';
+import uerda from '../images/team/uerda.png';
+
+//Sponsors
+import kris from '../images/team/kris.png';
+/*import alex from '../images/team/alex.png'; missing */
+import nadiaD from '../images/team/nadia-d.png';
+import preso from '../images/team/preso.png';
+import siel from '../images/team/siel.png';
+
+//Marketing
+import boyan from '../images/team/boyan.png';
+import asya from '../images/team/asya.png';
+import deni from '../images/team/deni.png';
+import didi from '../images/team/didi.png';
+import emma from '../images/team/emma.png';
+import emily from '../images/team/emily.png';
 import ilko from '../images/team/ilko.png';
+import ivy from '../images/team/ivy.png';
+import nikol from '../images/team/nikol.png';
+import petar from '../images/team/petar.png';
+import ted from '../images/team/ted.png';
+import teddy from '../images/team/teddy.png';
+
+//IT 
+import nick from '../images/team/nick.png';
+import mitko from '../images/team/mitko.png';
+import andi from '../images/team/andi.png';
+import vladin from '../images/team/vladin.png';
+
+//idk
+import flavia from '../images/team/flavia.png';
 
 const board = [
-	{ name: 'velko', img: velko },
-	{ name: 'emma', img: emma },
-	{ name: 'boyan', img: boyan },
-	{ name: 'nick', img: nick },
-	{ name: 'fatma', img: fatma },
-	{ name: 'ilko', img: ilko },
-	{ name: 'deni', img: deni },
+	{ name: 'Velislav Tsenov', img: velko, title: "President" },
+	{ name: 'Elena Ivanova', img: elito, title: "Vice President" },
+	{ name: 'Armela Gjylsheni', img: armela, title: "Vice President" },
+	{ name: 'Stanislav Todorov', img: stan, title: "Treasurer" }
 ];
-/*const speakersDept = [nick, fatma, ilko];
-const sponsorsDept = [deni, flavia, elito];
-const marketingDept = [];
-const itDept = [];*/
+
+const speakersDept = [
+	{ name: 'Fatma Xhafa', img: fatma },
+	{ name: 'Uerda Muca', img: uerda },
+	{ name: 'Darina Stanoilova', img: dari },
+	{ name: 'Nadia Traikova', img: nadiaT },
+	{ name: 'Karolina Kostadinova', img: karolina },
+	{ name: 'Key Prifti', img: key },
+	{ name: 'Aneta Gerginova', img: aneta },
+]
+
+const sponsorsDept = [
+	{ name: 'Kris Petrov', img: kris },
+	{ name: 'Nadia Dimitrova', img: nadiaD },
+	{ name: 'Preslav Vachev', img: preso },
+	{ name: 'Siel Rien', img: siel },
+]
+
+const marketingDept = [
+	{ name: 'Emma Kerencheva', img: emma },
+	{ name: 'Boyan Zlatarski', img: boyan },
+	{ name: 'Denislava Angelova', img: deni },
+	{ name: 'Ilia Dobrev', img: ilko },
+	{ name: 'Nikol Peeva', img: nikol },
+	{ name: 'Teddy Vasileva', img: teddy },
+	{ name: 'Petar Hristakiev', img: petar },
+	{ name: 'Asya Velichkova', img: asya },
+	{ name: 'Ivy Arabadzhieva', img: ivy },
+	{ name: 'Diana Radkova', img: didi },
+	{ name: 'Emily Timcheva', img: emily },
+	{ name: 'Teodora Danailova', img: ted }
+]
+
+const itDept = [
+	{ name: 'Nick Bruhnev', img: nick },
+	{ name: 'Dimitar Glavinkov', img: mitko },
+	{ name: 'Andrea Shipkovensky', img: andi },
+	{ name: 'Vladin Bonov', img: vladin }
+]
 
 function MembersSlide(props) {
 	return (
-		<>
-			<div className="row top-row">
+		<div className="members-slide">
+			<div className="row top-row" style={{background:`linear-gradient(to top, ${props.bgColor} 80%, transparent 20%)`}}>
 				{props.slide.map((member, index) => (
 					<>
 						<div className="col member-img-col text-center" style={{ height: '13em', width: '8em' }} key={index}>
@@ -47,18 +114,19 @@ function MembersSlide(props) {
 			</div>
 			<div className="row bot-row">
 				{props.slide.map((member, index) => (
-					<div className="col member-desc text-center" style={{ height: '13em', width: '8em'}} key={index}>
-						<div className="container">
-							<div className="member-name">{member.name}</div>
-						</div>
+					<div className="col member-desc text-center"  key={index}>
+						<div className="member-name">{member.name}</div>
+						<span className="member-title">{member?.title}</span>
+						<div className="line" style={{borderColor:props.bgColor}}></div>
+						<i class="fa fa-linkedin-square" aria-hidden="true"></i>
 					</div>
 				))}
 			</div>
-		</>
+		</div>
 	);
 }
 
-export default function Team() {
+function Dept(props) {
 
 	const chunkArray = (array, size) => {
 		let result = []
@@ -70,41 +138,70 @@ export default function Team() {
 	}
 
 	return (
+		<>
+			<h3 className="text-center">{props.teamName}</h3>
+
+			<MediaQuery maxWidth={360}>
+				<Carousel interval={null} style={{ marginBottom: '4em' }}>
+					{chunkArray(props.members, 1).map((group, index) => (
+						<Carousel.Item key={index}>
+							<MembersSlide slide={group} bgColor={props.bgColor} />
+						</Carousel.Item>
+					))}
+				</Carousel>
+			</MediaQuery>
+
+			<MediaQuery minWidth={361} maxWidth={768}>
+				<Carousel interval={null} style={{ marginBottom: '4em' }}>
+					{chunkArray(props.members, 2).map((group, index) => (
+						<Carousel.Item key={index}>
+							<MembersSlide slide={group} bgColor={props.bgColor} />
+						</Carousel.Item>
+					))}
+				</Carousel>
+			</MediaQuery>
+
+			<MediaQuery minWidth={769} maxWidth={999}>
+				<Carousel interval={null} style={{ marginBottom: '4em' }}>
+					{chunkArray(props.members, 3).map((group, index) => (
+						<Carousel.Item key={index}>
+							<MembersSlide slide={group} bgColor={props.bgColor} />
+						</Carousel.Item>
+					))}
+				</Carousel>
+			</MediaQuery>
+
+			<MediaQuery minWidth={1000}>
+				<Carousel interval={null} style={{ marginBottom: '4em' }}>
+					{chunkArray(props.members, 4).map((group, index) => (
+						<Carousel.Item key={index}>
+							<MembersSlide slide={group} bgColor={props.bgColor} />
+						</Carousel.Item>
+					))}
+				</Carousel>
+			</MediaQuery>
+		</>
+	);
+}
+
+
+export default function Team() {
+	return (
 		<div className="team">
-			<div className="container" style={{ paddingTop: '2em', paddingBottom: '2em' }} >
+			<div className="container" style={{ paddingTop: '2em', paddingBottom: '2em' }}>
 				<h2 className="team-title">
 					<span>Our Team</span>
 				</h2>
 
-				<MediaQuery maxWidth={768}>
-					<Carousel interval={null} style={{ marginBottom: '4em' }}>
-						{chunkArray(board, 2).map((group, index) => (
-							<Carousel.Item key={index}>
-								<MembersSlide slide={group} />
-							</Carousel.Item>
-						))}
-					</Carousel>
-				</MediaQuery>
+				<Dept teamName="The Board" members={board} bgColor="#fbbb06" />
 
-				<MediaQuery minWidth={769} maxWidth={999}>
-					<Carousel interval={null} style={{ marginBottom: '4em' }}>
-						{chunkArray(board, 3).map((group, index) => (
-							<Carousel.Item key={index}>
-								<MembersSlide slide={group} />
-							</Carousel.Item>
-						))}
-					</Carousel>
-				</MediaQuery>
+				<Dept teamName="Speakers Department" members={speakersDept} bgColor="#aa1638" />
 
-				<MediaQuery minWidth={1000}>
-					<Carousel interval={null} style={{ marginBottom: '4em' }}>
-						{chunkArray(board, 4).map((group, index) => (
-							<Carousel.Item key={index}>
-								<MembersSlide slide={group} />
-							</Carousel.Item>
-						))}
-					</Carousel>
-				</MediaQuery>
+				<Dept teamName="Sponsorship Department" members={sponsorsDept} bgColor=" #579ccb" />
+
+				<Dept teamName="Marketing Department" members={marketingDept} bgColor="#731c64" />
+
+				<Dept teamName="IT Team" members={itDept} bgColor="#2ECC40" />
 			</div>
 		</div>
 	);

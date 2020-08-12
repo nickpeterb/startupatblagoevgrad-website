9e3fbecb605cc454f43c5b7d934d7136109d020e
Nick Bruhnev <nicholaspeterb@gmail.com>
2020-08-12 11:20:04 -0500

    removed lazy load

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import './App.css';
 
 import NavBar from './components/NavBar.js';
@@ -6,25 +6,16 @@ import Masthead from './components/Masthead.js';
 import About from './components/About.js';
 import Testimonials from './components/Testimonials.js';
 import Events from './components/Events.js';
-//import Speakers from './components/Speakers.js';
+import Speakers from './components/Speakers.js';
 import Sponsors from './components/Sponsors.js';
-//import Team from './components/Team.js';
+import Team from './components/Team.js';
 import Contact from './components/Contact.js';
 
-const Speakers = React.lazy(() => import('./components/Speakers.js'));
-const Team = React.lazy(() => import('./components/Team.js'));
-/*
-function Loading() {
-  return (
-    <div>
-      <Suspense fallback={<div>Loading...</div>}>
-        <OtherComponent />
-      </Suspense>
-    </div>
-  );
-}*/
 
 function App() {
+
+  
+
   return (
     <div className="App">
       
@@ -37,16 +28,12 @@ function App() {
       <Testimonials />  
     
       <Events />
-    
-      <Suspense fallback={<div>Loading...</div>}>
-        <Speakers />
-      </Suspense>
+
+      <Speakers />
     
       <Sponsors />
       
-      <Suspense fallback={<div>Loading...</div>}>
-        <Team />
-      </Suspense>
+      <Team />
 
       <Contact />
 

6b3725c24e63985971070d42c9ba5e4bbdac74a9
Nick Bruhnev <nicholaspeterb@gmail.com>
2020-09-16 22:53:50 +0300

    translation + more linkedIns

--- src/components/Team.js
+++ src/components/Team.js
@@ -42,45 +42,84 @@ import mitko from '../images/team/mitko.png';
 import andi from '../images/team/andi.png';
 import vladin from '../images/team/vladin.png';
 
+const teamText = {
+	"title": {
+		"en": "Our Team",
+		"bg": "Нашия Екип"
+	},
+	"board": {
+		"en": "The Board",
+		"bg": "Борда"
+	},
+	"speakers": {
+		"en": "Speakers Department",
+		"bg": "Лектор Департмент"
+	},
+	"sponsorship": {
+		"en": "Sponsorship Department",
+		"bg": "Спонсор Департмент",
+	},
+	"marketing": {
+		"en": "Marketing Department",
+		"bg": "Маркетинг Департмент",
+	},
+	"it": {
+		"en": "IT Team",
+		"bg": "IT Отбор"
+	},
+	"positions": {
+		"president": {
+			"en": "President",
+			"bg": "Президент"
+		},
+		"vice": { "en": "Vice President", "bg": "Вицепрезидент" },
+		"speakers": {
+			"en": "Head of Speakers",
+			"bg": "Ръководител на Лекторите"
+		},
+		"sponsorship": { "en": "Head of Sponsorship", "bg": "Ръководител на Спонсорството" },
+		"marketing": { "en": "Head of Marketing", "bg": "Ръководител на Маркетинг" },
+	}
+}
 
 const board = [
-	{ name: 'Velislav Tsenov', img: velko, title: "President", linkedIn: "https://www.linkedin.com/in/velislavtsenov/" },
-	{ name: 'Elena Ivanova', img: elito, title: "Vice President", linkedIn: "https://www.linkedin.com/in/elena-ivanova-636178153/" },
-	{ name: 'Armela Gjylsheni', img: armela, title: "Vice President", linkedIn: "" },
-	{ name: 'Stanislav Todorov', img: stan, title: "Treasurer", linkedIn: "https://www.linkedin.com/in/stanislav-todorov-2a8a78198/" }
+	{ name: 'Velislav Tsenov', img: velko, title: { "en": "President", "bg": "Президент" }, linkedIn: "https://www.linkedin.com/in/velislavtsenov/" },
+	{ name: 'Elena Ivanova', img: elito, title: { "en": "Vice President, Head of Speakers", "bg": "Вицепрезидент, Ръководител на Лекторите" }, linkedIn: "https://www.linkedin.com/in/elena-ivanova-636178153/" },
+	{ name: 'Armela Gjylsheni', img: armela, title: { "en": "Vice President", "bg": "Вицепрезидент" }, linkedIn: "https://www.linkedin.com/in/armela-gjylsheni-404630153/" },
+	{ name: 'Stanislav Todorov', img: stan, title: { "en": "Treasurer", "bg": "Касиер" }, linkedIn: "https://www.linkedin.com/in/stanislav-todorov-2a8a78198/" }
 ];
 
 const speakersDept = [
-	{ name: 'Fatma Xhafa', img: fatma, linkedIn: "" },
+	{ name: 'Fatma Xhafa', img: fatma, linkedIn: "https://www.linkedin.com/in/fatma-xhafa-79446114b/" },
 	{ name: 'Uerda Muca', img: uerda, linkedIn: "https://www.linkedin.com/in/uerda-muca-340140135/" },
 	{ name: 'Darina Stanoilova', img: dari, linkedIn: "" },
 	{ name: 'Alexandra Dimcheva', img: alex, linkedIn: "https://www.linkedin.com/in/alexandra-dimcheva-b18866172/" },
-	{ name: 'Nadia Traikova', img: nadiaT, linkedIn: "" },
+	{ name: 'Nadia Traikova', img: nadiaT, linkedIn: "https://www.linkedin.com/in/nadia-traykova-124a70147/" },
 	{ name: 'Key Prifti', img: key, linkedIn: "" },
 	{ name: 'Aneta Gerginova', img: aneta, linkedIn: "" },
 ]
 
 const sponsorsDept = [
+	{ name: 'Nadia Dimitrova', img: nadiaD, title: { "en": "Head of Sponsorship", "bg": "Ръководител на Спонсорството" }, linkedIn: "http://linkedin.com/in/dimitrovani/" },
 	{ name: 'Kris Petrov', img: kris, linkedIn: "" },
-	{ name: 'Nadia Dimitrova', img: nadiaD, linkedIn: "http://linkedin.com/in/dimitrovani/" },
-	{ name: 'Siel Rien', img: siel, linkedIn: "" },
+	{ name: 'Siel Rien', img: siel, linkedIn: "https://www.linkedin.com/in/siel-rien-ab00a4170/" },
 	{ name: 'Flavia Gançi', img: flavia, linkedIn: "" },
 ]
 
 const marketingDept = [
-	{ name: 'Emma Kerencheva', img: emma, linkedIn: "" },
+	{ name: 'Emma Kerencheva', img: emma, title: { "en": "Head of Marketing", "bg": "Ръководител на Маркетинг" }, linkedIn: "" },
+	{ name: 'Emily Timcheva', img: emily, title: { "en": "Head of Marketing", "bg": "Ръководител на Маркетинг" }, linkedIn: "https://www.linkedin.com/in/emily-timcheva/" },
 	{ name: 'Boyan Zlatarski', img: boyan, linkedIn: "http://linkedin.com/in/boyan-zlatarski-5891261a6/" },
-	{ name: 'Denislava Angelova', img: deni, linkedIn: "" },
+	{ name: 'Denislava Angelova', img: deni, linkedIn: "https://www.linkedin.com/in/denislava-angelova/" },
 	{ name: 'Ilia Dobrev', img: ilko, linkedIn: "https://www.linkedin.com/in/ilia-dobrev/" },
 	{ name: 'Nikol Peeva', img: nikol, linkedIn: "" },
 	{ name: 'Teddy Vasileva', img: teddy, linkedIn: "" },
 	{ name: 'Petar Hristakiev', img: petar, linkedIn: "" },
 	{ name: 'Diana Radkova', img: didi, linkedIn: "" },
-	{ name: 'Emily Timcheva', img: emily, linkedIn: "https://www.linkedin.com/in/emily-timcheva/" }
 ]
 
 const itDept = [
-	{ name: 'Nick Bruhnev', img: nick, linkedIn: "" },
+	{ name: 'Nick Bruhnev', img: nick, linkedIn: "https://www.linkedin.com/in/nick-bruhnev/" },
 	{ name: 'Dimitar Glavinkov', img: mitko, linkedIn: "" },
 	{ name: 'Andrea Shipkovensky', img: andi, linkedIn: "" },
 	{ name: 'Vladin Bonov', img: vladin, linkedIn: "" }
@@ -105,9 +144,9 @@ function MembersSlide(props) {
 				{props.slide.map((member, index) => (
 					<div className="col member-desc text-center" key={index}>
 						<div className="member-name">{member.name}</div>
-						<span className="member-title">{member?.title}</span>
+						<span className="member-title">{member.title?.[props.lang]}</span>
 						<div className="line" style={{borderColor:props.bgColor}}></div>
-						{member.linkedIn !== "" ?
+						{ member.linkedIn !== "" ?
 							<i className="fa fa-linkedin-square" onClick={() => window.open(member.linkedIn,'_blank')}></i>
 						:
 							<div className="empty-square"></div>
@@ -152,7 +191,7 @@ function Dept(props) {
 				<Carousel interval={null} >
 					{chunkArray(props.members, 1).map((group, index) => (
 						<Carousel.Item key={index}>
-							<MembersSlide slide={group} bgColor={props.bgColor} />
+							<MembersSlide slide={group} bgColor={props.bgColor} lang={props.lang} />
 						</Carousel.Item>
 					))}
 				</Carousel>
@@ -162,7 +201,7 @@ function Dept(props) {
 				<Carousel interval={null} >
 					{chunkArray(props.members, 2).map((group, index) => (
 						<Carousel.Item key={index}>
-							<MembersSlide slide={group} bgColor={props.bgColor} />
+							<MembersSlide slide={group} bgColor={props.bgColor} lang={props.lang}/>
 						</Carousel.Item>
 					))}
 				</Carousel>
@@ -172,7 +211,7 @@ function Dept(props) {
 				<Carousel interval={null} >
 					{chunkArray(props.members, 3).map((group, index) => (
 						<Carousel.Item key={index}>
-							<MembersSlide slide={group} bgColor={props.bgColor} />
+							<MembersSlide slide={group} bgColor={props.bgColor} lang={props.lang}/>
 						</Carousel.Item>
 					))}
 				</Carousel>
@@ -182,7 +221,7 @@ function Dept(props) {
 				<Carousel className={hideCarousel} interval={null} >
 					{chunkArray(props.members, membersLgDevice).map((group, index) => (
 						<Carousel.Item key={index}>
-							<MembersSlide slide={group} bgColor={props.bgColor} hideIndicators={true}/>
+							<MembersSlide slide={group} bgColor={props.bgColor} hideIndicators={true} lang={props.lang}/>
 						</Carousel.Item>
 					))}
 				</Carousel>
@@ -192,23 +231,23 @@ function Dept(props) {
 }
 
 
-export default function Team() {
+export default function Team(props) {
 	return (
 		<div className="team">
 			<div className="container-fluid" style={{ paddingTop: '2em', paddingBottom: '2em' }}>
 				<h2 className="team-title text-center">
-					<span>Our Team</span>
+					<span>{teamText.title[props.lang]}</span>
 				</h2>
 				
-				<Dept teamName="The Board" members={board} bgColor="#fbbb06" />
+				<Dept teamName={teamText.board[props.lang]} members={board} bgColor="#fbbb06" lang={props.lang}/>
 
-				<Dept teamName="Speakers Department" members={speakersDept} bgColor="#aa1638" />
+				<Dept teamName={teamText.speakers[props.lang]} members={speakersDept} bgColor="#aa1638" lang={props.lang}/>
 
-				<Dept teamName="Sponsorship Department" members={sponsorsDept} bgColor=" #579ccb" />
+				<Dept teamName={teamText.sponsorship[props.lang]} members={sponsorsDept} bgColor=" #579ccb" lang={props.lang}/>
 
-				<Dept teamName="Marketing Department" members={marketingDept} bgColor="#731c64" />
+				<Dept teamName={teamText.marketing[props.lang]} members={marketingDept} bgColor="#731c64" lang={props.lang}/>
 
-				<Dept teamName="IT Team" members={itDept} bgColor="#2ECC40" />
+				<Dept teamName={teamText.it[props.lang]} members={itDept} bgColor="#2ECC40" lang={props.lang}/>
 			</div>
 		</div>
 	);

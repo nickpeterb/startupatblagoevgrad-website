883021b31b503fc8aedf0b223b3038a1f2423e51
Nick Bruhnev <nicholaspeterb@gmail.com>
2020-10-20 01:15:58 +0300

    conf page

---
+++ src/components/Conference/Conference.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import ConfSpeakers from './ConfSpeakers.js';
+
+export default function Conference() {
+    return(
+        <>
+            <ConfSpeakers />
+        </>
+    );
+}

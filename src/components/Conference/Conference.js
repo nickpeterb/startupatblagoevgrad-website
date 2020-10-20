d4148b22d825f61600fe6e89fce8ac348a718937
Nick Bruhnev <nicholaspeterb@gmail.com>
2020-10-21 02:41:46 +0300

    added about and masthead

--- src/components/Conference/Conference.js
+++ src/components/Conference/Conference.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import ConfAbout from './ConfAbout.js';
+import ConfMasthead from './ConfMasthead.js';
 import ConfSpeakers from './ConfSpeakers.js';
 
 export default function Conference() {
     return(
-        <>
+        <div style={{backgroundColor: "#222", color: "white"}}>
+            <ConfMasthead />
+            <ConfAbout />
             <ConfSpeakers />
-        </>
+        </div>
     );
 }

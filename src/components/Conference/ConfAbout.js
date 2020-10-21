94092d32d3110b49d848527dbdc9b7028aa27dca
Nick Bruhnev <nicholaspeterb@gmail.com>
2020-10-21 19:09:18 +0300

    rm video

--- src/components/Conference/ConfAbout.js
+++ src/components/Conference/ConfAbout.js
@@ -8,7 +8,7 @@ export default function ConfAbout() {
             <div className="container">
                 <h2 className="conf-about-title text-center mb-5"><span>About the Event</span></h2>
 
-                <div className="row">
+                <div className="row align-items-center">
                     <div className="col-xl">
                         <div className="conf-about-text">
                             StartUP Conference is an annual format that brings together entrepreneurs and world-renowned business leaders to share their outlook on the path of success, diving deep into the ups and downs and the dos and don’ts of the journey through the business world.
@@ -20,9 +20,9 @@ export default function ConfAbout() {
                             StartUP@Blagoevgrad is a student-run NGO under the patronage of the American University in Bulgaria. Established in 2012, the organization has helped students in the Blagoevgrad region develop their own businesses by conducting multiple competitions, conferences, and workshops.
                             </div>
                     </div>
-                    <div className="col-md">
-                        <iframe loading="lazy" title="conf-about-vid" className="conf-about-vid" width="100%" height="100%" src="https://www.youtube.com/embed/WeSSGW0bkEs" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true}></iframe>
-                    </div>
+                    {/*<div className="col-md" >
+                        <img src={pepoStage} className="pepo-img" alt="" />
+                    </div>*/}
                 </div>
 
 

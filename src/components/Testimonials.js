fa719fc371559ac52acc340347bbbcb26d00b459
nickpeterb <nicholaspeterb@gmail.com>
2020-11-06 15:12:28 +0200

    new tesimonials

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -4,21 +4,57 @@ import Carousel from 'react-bootstrap/Carousel';
 import './styles/Testimonials.css';
 
 import kirilImg from '../images/testimonials/kiril-z.png';
+import victorImg from '../images/testimonials/victor.jpg';
+import miroImg from '../images/testimonials/miroslav.jpg';
+import georgiImg from '../images/testimonials/georgi.jpg'
+import vladImg from '../images/testimonials/vladi.jpg';
+import deniImg from '../images/testimonials/deni.jpg';
+
 
 const testimonialsText = {
 	"title": {
-		"en": "Testimonial",
+		"en": "Testimonials",
 		"bg": "Какво Мислят Нашите Гости"
 	}
 }
 
 const testimonialsList = [
 	{
+		name: 'Victor Penev',
+		img: victorImg,
+		text: "It was a pleasure working with the StartUp Conference 2020 team. The team was very responsive, willing to address challenges right away and find a solution so that the best experience is brought to the audience. I would work with them again in a heartbeat.",
+		cite: 'Speaker at StartUP Conference 2020'
+	},
+	{
+		name: 'Miroslav Miroslavov',
+		img: miroImg,
+		text: "The Startup Conference 2020 was great, very well organized, especially in such a difficult environment. The team went above and beyond to make it happen and make it really high quality.",
+		cite: 'Speaker at StartUP Conference 2020'
+	},
+	{
+		name: 'Vlad Dramaliev',
+		img: vladImg,
+		text: "One of the most well-organized online conferences I have had the honor to attend. The Blagoevgrad startup community is one of the most vibrant ones in the country.",
+		cite: 'Speaker at StartUP Conference 2020'
+	},
+	{
+		name: 'Denitsa Simeonova',
+		img: deniImg,
+		text: "Being part of StartUP Conference 2020 was a very positive experience for me. A startup event organised from students and for students to very high standards is an inspiration to all of us who are part of the entrepreneurial ecosystem and gives us a chance to contribute to its development.",
+		cite: 'Speaker at StartUP Conference 2020'
+	},
+	{
+		name: 'Georgi Stoyanov',
+		img: georgiImg,
+		text: "Every touch with Startup@Blagoevgrad up to now has brought me much inspiration. I adore this smart and youthful team of people whose passion with entrepreneurship and knowledge is so beautiful. May this enthusiasm be never-ending, Startup Team!",
+		cite: 'Moderator at StartUP Conference 2020'
+	},
+	{
 		name: 'Kiril Zhelyazkov',
 		img: kirilImg,
 		text: "Big thanks to @StartUPBlagoevgrad for organizing the StartUP Weekend. To me it was an excellent and refreshing opportunity to get away of everyday life and try out what it's like to be one of the creators of a disruptive business idea.",
-		cite: {"en": "1st Place Winner at StartUP Weekend 2018", "bg": "1во Място в StartUP Уикенд 2018"}
-	}
+		cite: "1st Place Winner at StartUP Weekend 2018",
+	}, 
 ]
 
 export default function Testimonials(props) {
@@ -30,9 +66,10 @@ export default function Testimonials(props) {
 				</h2>
 			</div>
 
+			<Carousel interval={7000} >
 			{ testimonialsList.map((testimonial, index) => (
-				<Carousel interval={7000} key={index}>
-				<Carousel.Item>
+				
+				<Carousel.Item key={index}>
 					<div className="container">
 						<div className="row align-items-center mb-5">
 						    <div className="col-md">
@@ -56,15 +93,16 @@ export default function Testimonials(props) {
 									<footer className="blockquote-footer">
                                         {testimonial.name}
                                         <br/>
-										<cite>{testimonial.cite[props.lang]}</cite>
+										<cite>{testimonial.cite}</cite>
 									</footer>
 								</blockquote>
 							</div>
 						</div>
 					</div>
 				</Carousel.Item>
-			</Carousel>
+			
 			))}
+			</Carousel>
 			
 		</div>
 	);

e869329624f9b672ebfb5f7f0987715756d5abf5
nickpeterb <nicholaspeterb@gmail.com>
2020-11-24 21:02:12 +0200

    scroll to top on load

--- src/components/Conference2020/Conference.js
+++ src/components/Conference2020/Conference.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import ConfAbout from './ConfAbout.js';
 import ConfMasthead from './ConfMasthead.js';
 import ConfSpeakers from './ConfSpeakers.js';
 
 export default function Conference() {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
     return(
         <div style={{backgroundColor: "#222", color: "white"}}>
             <ConfMasthead />

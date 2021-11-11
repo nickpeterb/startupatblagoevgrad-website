0bf6eea77c26026091499a54b542a9b852047439
Nick Bruhnev <nicholaspeterb@gmail.com>
2021-11-11 13:27:05 +0200

    lazy load countdown & loading spinner

--- src/components/Conference2021/MainComponent/Conf2021.js
+++ src/components/Conference2021/MainComponent/Conf2021.js
@@ -2,24 +2,36 @@ import React, { useEffect } from "react";
 import poster from "../media/poster.JPG";
 import "./Conf2021.css";
 
-import Countdown from "../CountdownComponent/Countdown";
 import Panels from "../PanelsComponent/Panels";
+const Countdown = React.lazy(() => import("../CountdownComponent/Countdown"));
+
+const Loader = () => {
+    return (
+        <div class="spinner">
+            <div class="lds-dual-ring"></div>
+        </div>
+    );
+};
 
 export default function Conf2021() {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
 
-  return (
-    <div class="conf2021">
-      <div>
-        <img src={poster} style={{ width: "100%" }} alt=""></img>
-        <Countdown
-          timeTillDate="11 21 2021, 14:00 pm"
-          timeFormat="MM DD YYYY, h:mm a"
-        />
-        <Panels />
-      </div>
-    </div>
-  );
+    return (
+        <div class="conf2021">
+            <div>
+                <img src={poster} alt=""></img>
+                <div>
+                    <React.Suspense fallback={<Loader />}>
+                        <Countdown
+                            timeTillDate="11 21 2021, 14:00 pm"
+                            timeFormat="MM DD YYYY, h:mm a"
+                        />
+                    </React.Suspense>
+                    <Panels />
+                </div>
+            </div>
+        </div>
+    );
 }

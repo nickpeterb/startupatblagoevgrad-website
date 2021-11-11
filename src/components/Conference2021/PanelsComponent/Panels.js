9b63eeba73fb1d2ae5777d8789e489665d327418
Nick Bruhnev <nicholaspeterb@gmail.com>
2021-11-11 13:27:21 +0200

    basic syling for panels

--- src/components/Conference2021/PanelsComponent/Panels.js
+++ src/components/Conference2021/PanelsComponent/Panels.js
@@ -3,40 +3,62 @@ import "./Panels.css";
 import panels from "./panelsData";
 
 export default function Conf2021() {
-  return (
-    <div className="panels" style={{ color: "white" }}>
-      <h2 className="speakers">
-        <span>Speakers</span>
-      </h2>
-      <div className="panel">
-        {panels.panel1.map((speaker) => (
-          <div>
-            {speaker.speakerName} {speaker.company} {speaker.businessSphere}{" "}
-            {speaker.theme} {speaker.duration} {speaker.eventType}{" "}
-            {speaker.socialLink}
-          </div>
-        ))}
-      </div>
+    return (
+        <div className="panels">
+            <div className="panel-wrapper">
+                <h2>Panel 1</h2>
+                <div className="panel">
+                    {panels.panel1.map((speaker) => (
+                        <Speaker speaker={speaker} />
+                    ))}
+                </div>
+            </div>
 
-      <div className="panel">
-        {panels.panel2.map((speaker) => (
-          <div>
-            {speaker.speakerName} {speaker.company} {speaker.businessSphere}{" "}
-            {speaker.theme} {speaker.duration} {speaker.eventType}{" "}
-            {speaker.socialLink}
-          </div>
-        ))}
-      </div>
+            <div className="panel-wrapper">
+                <h2>Panel 2</h2>
+                <div className="panel">
+                    {panels.panel2.map((speaker) => (
+                        <Speaker speaker={speaker} />
+                    ))}
+                </div>
+            </div>
 
-      <div className="panel">
-        {panels.panel3.map((speaker) => (
-          <div>
-            {speaker.speakerName} {speaker.company} {speaker.businessSphere}{" "}
-            {speaker.theme} {speaker.duration} {speaker.eventType}{" "}
-            {speaker.socialLink}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+            <div className="panel-wrapper">
+                <h2>Panel 3</h2>
+                <div className="panel">
+                    {panels.panel3.map((speaker) => (
+                        <Speaker speaker={speaker} />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function Speaker({ speaker }) {
+    return (
+        <div className="speaker">
+            <div className="header">
+                <div>
+                    {speaker.duration} min {speaker.eventType}
+                </div>
+            </div>
+            <div className="name">{speaker.speakerName}</div>
+            <div className="footer">
+                <i
+                    className={
+                        "fa fa-" + socialButton(speaker.socialLink) + "-square"
+                    }
+                    onClick={() => window.open(speaker.socialLink, "_blank")}
+                ></i>
+                <div>{speaker.company}</div>
+                <div className="businessSphere">{speaker.businessSphere}</div>
+            </div>
+        </div>
+    );
+}
+
+function socialButton(link) {
+    if (link.includes("linkedin")) return "linkedin";
+    if (link.includes("facebook")) return "facebook";
 }

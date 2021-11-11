00eb0390603e0a0385397a2719bf6a9e693107b7
Nick Bruhnev <nicholaspeterb@gmail.com>
2021-11-11 14:32:05 +0200

    made header nicer

--- src/components/Conference2021/MainComponent/Conf2021.js
+++ src/components/Conference2021/MainComponent/Conf2021.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import poster from "../media/poster.JPG";
+import poster from "../media/poster.png";
 import "./Conf2021.css";
 
 import Panels from "../PanelsComponent/Panels";

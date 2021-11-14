1615beed9656a31f00b047b40a5999248c1a015f
Nick Bruhnev <nicholaspeterb@gmail.com>
2021-11-14 14:52:03 +0200

    removed countdown loading spinner

--- src/components/Conference2021/MainComponent/Conf2021.js
+++ src/components/Conference2021/MainComponent/Conf2021.js
@@ -5,14 +5,6 @@ import Header from "../HeaderComponent/Header";
 import Panels from "../PanelsComponent/Panels";
 const Countdown = React.lazy(() => import("../CountdownComponent/Countdown"));
 
-const Loader = () => {
-    return (
-        <div class="spinner">
-            <div class="lds-dual-ring"></div>
-        </div>
-    );
-};
-
 export default function Conf2021() {
     return (
         <div class="conf2021">
@@ -26,7 +18,7 @@ export default function Conf2021() {
                             <div className="glimpse">A glimpse into</div>
                             <div className="future">The Future</div>
 
-                            <React.Suspense fallback={<Loader />}>
+                            <React.Suspense fallback={<></>}>
                                 <Countdown
                                     timeTillDate="11 21 2021, 14:00 pm"
                                     timeFormat="MM DD YYYY, h:mm a"

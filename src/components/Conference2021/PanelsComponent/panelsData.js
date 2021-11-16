676b529bfd8f20680f52ea4b8655746c65c82e89
Denis7J <45210930+Denis7J@users.noreply.github.com>
2021-11-16 18:47:59 +0200

    Update speakers

--- src/components/Conference2021/PanelsComponent/panelsData.js
+++ src/components/Conference2021/PanelsComponent/panelsData.js
@@ -7,6 +7,10 @@ import spas from "../media/speakers/Spas_Kerimov.png";
 import teod from "../media/speakers/Teodosi_Teodosiev.png";
 import vlad from "../media/speakers/Vladimir_Tabutov.png";
 import xav from "../media/speakers/Xavier_Marcenac.png";
+import dent from "../media/speakers/Denitza_Tyufekchieva.png";
+import ventt from "../media/speakers/Ventsislav_Topuzov.png";
+import vikm from "../media/speakers/Viktoriya_Markileva.png";
+import yonko from "../media/speakers/Yonko_Chuklev.png";
 
 const panels = {
     panel1: [
@@ -15,7 +19,7 @@ const panels = {
             company: "Nexo",
             businessSphere: "Blockchain, Crypto",
             theme: "Riding on the Crest of the Wave",
-            duration: 20,
+            duration: 25,
             eventType: "Speech",
             socialLink: "https://www.facebook.com/georgemanolov",
             img: georgim,
@@ -35,32 +39,42 @@ const panels = {
             company: "MYX",
             businessSphere: "DeepTech, AI",
             theme: "",
-            duration: 10,
-            eventType: "Speech",
+            duration: 15,
+            eventType: "Interview",
             socialLink: "https://www.linkedin.com/in/dean-bezlov-myx/",
             img: dean,
         },
+        {
+            speakerName: "Denitza Tyufekchieva",
+            company: "Propy",
+            businessSphere: "Blockchain in Real Estate - Prop Tech",
+            theme: "",
+            duration: 15,
+            eventType: "Interview",
+            socialLink: "	https://www.linkedin.com/in/denitza-tyufekchieva-18890b50/",
+            img: dent,
+        },
     ],
     panel2: [
-        {
-            speakerName: "Spas Kerimov",
-            company: "Printivo",
-            businessSphere: "Bio Tech",
+         {
+            speakerName: "Vladimir Tabutov",
+            company: "Hec Solar",
+            businessSphere: "Renewable Energy",
             theme: "",
-            duration: 15,
-            eventType: "Interview",
-            socialLink: "https://www.linkedin.com/in/spas-kerimov/",
-            img: spas,
+            duration: 10,
+            eventType: "Speech",
+            socialLink: "https://www.linkedin.com/in/vladimir-tabutov-44a6026/",
+            img: vlad,
         },
         {
-            speakerName: "Georgi Kostadinov",
-            company: "Kelvin Health",
-            businessSphere: "Bio Tech",
+            speakerName: "Ventsislav Topuzov",
+            company: "ROITI",
+            businessSphere: "Renewable Energy",
             theme: "",
             duration: 15,
             eventType: "Interview",
-            socialLink: "https://www.linkedin.com/in/georgi-kostadinov/",
-            img: georgik,
+            socialLink: "https://www.linkedin.com/in/ventsislavtopuzov/",
+            img: ventt,
         },
         {
             speakerName: "Neville Mchina",
@@ -82,17 +96,37 @@ const panels = {
             socialLink: "https://www.linkedin.com/in/xaviermarcenac/",
             img: xav,
         },
+        {
+            speakerName: "Viktoriya Markileva",
+            company: "EnduroSat",
+            businessSphere: "Space",
+            theme: "",
+            duration: 10,
+            eventType: "Presentation",
+            socialLink: "https://www.linkedin.com/in/viktoriya-markileva/",
+            img: vikm,
+        },
     ],
     panel3: [
         {
-            speakerName: "Vladimir Tabutov",
-            company: "Hec Solar",
-            businessSphere: "Renewable Energy",
+            speakerName: "Spas Kerimov",
+            company: "Printivo",
+            businessSphere: "Bio Tech",
             theme: "",
-            duration: 10,
-            eventType: "Speech",
-            socialLink: "https://www.linkedin.com/in/vladimir-tabutov-44a6026/",
-            img: vlad,
+            duration: 15,
+            eventType: "Interview",
+            socialLink: "https://www.linkedin.com/in/spas-kerimov/",
+            img: spas,
+        },
+        {
+            speakerName: "Georgi Kostadinov",
+            company: "Kelvin Health",
+            businessSphere: "Bio Tech",
+            theme: "",
+            duration: 15,
+            eventType: "Interview",
+            socialLink: "https://www.linkedin.com/in/georgi-kostadinov/",
+            img: georgik,
         },
         {
             speakerName: "Teodosi Teodosiev",
@@ -103,6 +137,16 @@ const panels = {
             eventType: "Interview",
             socialLink: "https://www.facebook.com/teodosy",
             img: teod,
+        },
+        {
+            speakerName: "Yonko Chuklev",
+            company: "PARA",
+            businessSphere: "Robotics and Automation",
+            theme: "",
+            duration: 15,
+            eventType: "Interview",
+            socialLink: "https://www.linkedin.com/in/yonko-chuklev-a7733568/",
+            img: yonko,
         },
     ],
 };
